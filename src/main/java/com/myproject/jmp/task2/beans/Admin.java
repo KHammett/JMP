2d71b1273cab5579daebae4e6098d8165262fe86
KHammett <khammett@2ch.hk>
2015-12-14 02:10:31 +0300

    Task 2

--- src/main/java/com/myproject/jmp/task2/beans/Admin.java
+++ src/main/java/com/myproject/jmp/task2/beans/Admin.java
@@ -1,7 +1,20 @@
 package com.myproject.jmp.task2.beans;
 
+import java.util.List;
+
 /**
  * Created by firel on 14.12.2015.
  */
 public class Admin extends AuthorizedUser {
+    public void addNews(News news) {
+
+    }
+
+    public void editRole(User user) {
+
+    }
+
+    public List<User> createAttendiesList(Test test) {
+        return null;
+    }
 }
--- src/main/java/com/myproject/jmp/task2/beans/Test.java
+++ src/main/java/com/myproject/jmp/task2/beans/Test.java
@@ -4,5 +4,5 @@ package com.myproject.jmp.task2.beans;
  * Created by firel on 14.12.2015.
  */
 public class Test {
-
+    private String name;
 }
