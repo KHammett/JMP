23d7f0968f177b0ace2859d1430facd7cfefe0d4
KHammett <khammett@2ch.hk>
2015-12-14 01:28:26 +0300

    Task 2

---
+++ src/main/java/com/myproject/jmp/task2/beans/Admin.java
@@ -0,0 +1,7 @@
+package com.myproject.jmp.task2.beans;
+
+/**
+ * Created by firel on 14.12.2015.
+ */
+public class Admin extends User {
+}
---
+++ src/main/java/com/myproject/jmp/task2/beans/Role.java
@@ -0,0 +1,8 @@
+package com.myproject.jmp.task2.beans;
+
+/**
+ * Created by firel on 14.12.2015.
+ */
+public enum Role {
+    DEFAULT, AUTHORIZED, TRAINER, ADMIN
+}
